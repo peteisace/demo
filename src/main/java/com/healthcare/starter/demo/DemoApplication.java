43ac4bcc7da41a8c566d5ccda6a81d33babbad34
pete <peteisace@gmail.com>
2022-03-10 14:02:48 +0700

    Builder

--- src/main/java/com/healthcare/starter/demo/DemoApplication.java
+++ src/main/java/com/healthcare/starter/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.healthcare.starter.demo;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	private ParentRepository parentRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		val myClass = MyBigClass.builder()
+			.field1("hello")
+			.field2("bye")
+			.field3(12)
+			.field4(13)
+			.build();
+	}
 }
---
+++ src/main/java/com/healthcare/starter/demo/MyBigClass.java
@@ -0,0 +1,16 @@
+package com.healthcare.starter.demo;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MyBigClass {
+
+    private final String field1;
+    private final String field2;
+    private final int field3;
+    private final int field4;
+    private final long field5;
+    private final long field6;
+}
